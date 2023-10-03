6fa3835a515622611e44b036e8bd4c5267ac17ac
nishikei <c0a22115f4@edu.teu.ac.jp>
2023-10-03 15:23:09 +0900

    練習５

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -45,9 +45,9 @@ def main():
             if event.type == pg.QUIT: 
                 return
 
-        #if kk_rct.colliderect(bd_rct):
-        #    print("ゲームオーバー")
-        #    return
+        if kk_rct.colliderect(bd_rct):
+            print("ゲームオーバー")
+            return
         screen.blit(bg_img, [0, 0])
 
         """こうかとん"""
